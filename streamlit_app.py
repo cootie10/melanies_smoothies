6c556c3942b0e8a4a235bfadebe73c293d4202fc
Chris Coote <22786368+cootie10@users.noreply.github.com>
2024-05-25 14:21:04 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order+'!', icon="✅")
 
+#new section to display fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
